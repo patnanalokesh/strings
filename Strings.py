75b05d0eaae1f0f652957fc432de93292c6f362a
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-07-20 17:00:26 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,44 @@
+# print(type(2.56))
+# print(type(5))
+
+# print('hello world')
+# print("hello world")
+# print('''hello world''')
+# print("""hello world""")
+
+
+''' working with int,float and str'''
+# print(54+2.1)
+# print(type(26+3.8))
+# print(type(6.23+3.25))
+# print(type(6.4+2.6))
+# print('256'+2.6)
+# print('54'+54)
+
+# print('hello'+54)
+# print('hello'+2.5)
+
+# print(1,2,3,4,5,6)
+# print(1,2,3,4,5,sep='')
+# print(1,2,3,4,5,sep=' ')
+# print(1,2,3,4,5,sep='   ')
+# print(1,2,3,4,5,sep='-')
+
+# print('hello'+'world')
+# print('hello '+'world')
+# print('hello'+' world')
+# print('hello','world')
+# print('hi','lokesh')
+
+# print('hello'+''+'lokesh')
+# print('hello'+' '+'lokesh')
+# print("hello"+"  "+"lokesh")
+# print("hello"+"-"+"lokesh")
+
+# print('lokesh'*4)
+# print('lokesh '*4)
+# print('lokesh\n'*4)
+# print('lokesh\n'*4,end='')
+# print('lokesh\n'*7,end=' ')
+# print('lokesh\n',end='')
+# print('lokesh/',sep='')
